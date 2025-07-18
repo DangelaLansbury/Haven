0113e52b9d24e19bd0173a711f0f7169419b4b44
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2025-07-18 05:01:26 -0400

    Modules for form and demo

--- apps/web/src/components/DemoForm.tsx
+++ apps/web/src/components/DemoForm.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import demoStyles from '../css/Form.module.css';
+import formStyles from '../css/Form.module.css';
 import FormInput from './FormInput';
 
 const DemoForm: React.FC = () => {
   const noop = () => {};
   return (
     <>
-      <h1 className={demoStyles.formHeader}>{'Demo Form'}</h1>
+      <h1 className={formStyles.formHeader}>{'Demo Form'}</h1>
       <p className={demoStyles.formDescription}>{`Use the QR code in your browser to take a picture of this form with your mobile device.`}</p>
       <div className={demoStyles.formContainer}>
         <form className={demoStyles.demoForm}>
