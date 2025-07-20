de188ce4f49568e425c2156219a535c2695f0a6f
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2025-07-20 17:33:19 -0400

    Demo form component starter

--- apps/web/src/components/DemoForm.tsx
+++ apps/web/src/components/DemoForm.tsx
@@ -3,12 +3,26 @@ import demoStyles from '../css/DemoForm.module.css';
 import formStyles from '../css/Form.module.css';
 import FormInput from './FormInput';
 
+interface DemoFormRowProps {
+  label: string;
+  value: string;
+}
+
+const DemoFormRow: React.FC<DemoFormRowProps> = ({ label, value }) => {
+  return (
+    <div className={demoStyles.demoFormRow}>
+      <div className={demoStyles.demoFormLabel}>{label}</div>
+      <div className={demoStyles.demoFormValue}>{value}</div>
+    </div>
+  );
+};
+
 const DemoForm: React.FC = () => {
   const noop = () => {};
   return (
     <>
       <h1 className={formStyles.formHeader}>{'Demo Form'}</h1>
-      <p className={demoStyles.formDescription}>{`Use the QR code in your browser to take a picture of this form with your mobile device.`}</p>
+      <p className={demoStyles.formDescription}>{`Use your device to take a picture and populate the Tax Ghost form.`}</p>
       <div className={demoStyles.formContainer}>
         <form className={demoStyles.demoForm}>
           <FormInput label="Gross Income" value={'$50,000'} readonly onChange={noop} />
