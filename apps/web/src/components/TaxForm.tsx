630113b22d62034f3328fe15161678787768cbc0
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2025-07-23 08:40:30 -0400

    Better container styling

--- apps/web/src/components/TaxForm.tsx
+++ apps/web/src/components/TaxForm.tsx
@@ -11,9 +11,9 @@ interface TaxFormProps {
 const TaxForm: React.FC<TaxFormProps> = ({ title, description, formData, setFormData }) => {
   return (
     <>
-      <h1 className={formStyles.formHeader}>{title || 'Tax Ghost'}</h1>
-      {description && <p className={formStyles.formDescription}>{description}</p>}
       <div className={formStyles.formContainer}>
+        <h1 className={formStyles.formHeader}>{title || 'Tax Ghost'}</h1>
+        {description && <p className={formStyles.formDescription}>{description}</p>}
         <form className={formStyles.taxForm}>
           <FormInput label="Gross Income" value={formData.grossIncome || ''} placeholder={'00,000.00'} onChange={(value) => setFormData({ ...formData, grossIncome: value })} />
           <FormInput label="General Deductions" value={formData.generalDeductions || ''} placeholder={'00,000.00'} onChange={(value) => setFormData({ ...formData, generalDeductions: value })} />
